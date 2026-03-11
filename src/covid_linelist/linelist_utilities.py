5ef7568e6d4ce21405b20e6ab5d5da14907691d4
Deborah Greer <deborah.greer@ukhsa.gov.uk>
2026-03-11 13:55:20 +0000

    chore: Update over_1pct var name in get_lineages_to_protect to over_threshold - Remove reference to specific threshold value to make var names clearer and   allow for threshold value changes to now need further var name changes

--- src/covid_linelist/linelist_utilities.py
+++ src/covid_linelist/linelist_utilities.py
@@ -188,12 +188,10 @@ def get_lineages_to_protect(counts_by_week_df: pd.DataFrame, timeframe_length: i
                                    totals_col='seq_count',
                                    min_level = 2,
                                    pango_aliases=pango_dict)
-        over_1pct = lc_week.collapse_based_on_pct(percent_threshold)
-        to_protect_week = over_1pct.groupby('collapsed').sum('pct_of_week').reset_index().query('pct_of_week >= 1').collapsed.unique().tolist()
+        over_threshold = lc_week.collapse_based_on_pct(percent_threshold)
+        to_protect_week = over_threshold.groupby('collapsed').sum('pct_of_week').reset_index().query('pct_of_week >= 1').collapsed.unique().tolist()
         to_protect_collapsed += to_protect_week
 
     to_protect_collapsed = set(to_protect_collapsed)
     return list(to_protect_collapsed)
-
-
 
