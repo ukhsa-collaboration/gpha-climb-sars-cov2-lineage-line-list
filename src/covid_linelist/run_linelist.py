81f5d137cff05b85e1603f58e9abf45854580aaa
Deborah Greer <deborah.greer@ukhsa.gov.uk>
2026-03-13 22:24:41 +0000

    feat: Adding updated run_linelist script - Change to 2 week blocks for prevalence calculations - Update references to week to period - Add collapse_resursively_to_at_least_n to do lineage groupings - Add lineage groups to sample metadata file - Note - needs further testing with full dataset

--- src/covid_linelist/run_linelist.py
+++ src/covid_linelist/run_linelist.py
@@ -60,8 +60,7 @@ def main():
     args = get_args()  # noqa: F841
 
     # Get today's date
-    today_date = dt.date.today().strftime("%Y-%m-%d")
-
+    today_date = dt.date.today()
     # Make output dir
     out_dir = Path(args.output) / f"{today_date}_covid_linelist"
     Path(out_dir).mkdir(parents=False, exist_ok=True)
@@ -70,6 +69,7 @@ def main():
     log_file = Path(out_dir) / f"{today_date}_covid_linelist_logfile.txt"
     set_up_logger(log_file)
 
+    ## Prepare sample data for prevalence calculations
     # Read in required columns from input csv with lineage designations
     try:
         lineage_df = llu.read_lineage_reports_csv(args.input)
@@ -78,67 +78,86 @@ def main():
         logging.error("Lineage reports file %s not found, exiting program", args.input)
         exitcode = 1
         return exitcode
-    # Check and fill collection date column.
-    # NOTE: - fills empty dates with today's date - decide if correct behaviour
+    # Get Monday of current week as end of range for any time periods calculated.
+    range_end = today_date - dt.timedelta(days=today_date.weekday())
+    # Check and fill specimen date column.
+    # NOTE: - fills empty dates with Monday of current week - decide if correct behaviour
     lineage_df = llu.check_and_update_specimen_dates(
-        lineage_df=lineage_df, specimen_date=today_date, fill_blanks=True
+        lineage_df=lineage_df,
+        specimen_date=range_end,
+        fill_blanks=True
     )
-    #  Add column to df with week begin date
-    lineage_df = llu.add_week_begin_column(lineage_df=lineage_df)
-
     #  Filter rows to required timeframe.
-    filtered_df = llu.filter_lineage_df_to_date_cutoff(
-        lineage_df=lineage_df, filter_start_date=today_date, previous_weeks_to_include=52
-    )  # TODO - add weeks to include to config if reimplement?
-
-    ## Linelist generation
-    # Group data to get lineage counts per week
-    counts_by_week_df = llu.get_lineage_counts_per_week(filtered_df)
-    # Add timeframe to protect lineages in as column
-    counts_by_week_df = llu.add_timeframe_to_protect_column(
-        counts_by_week_df=counts_by_week_df, timeframe_start=today_date, timeframe_length=6
+    lineage_df = llu.filter_lineage_df_to_date_cutoff(
+        lineage_df=lineage_df,
+        filter_end_date=range_end,
+        previous_weeks_to_include=52 # TODO - add weeks to include to config if reimplement?
     )
+    # Add column to df with reporting periods 
+    lineage_df = llu.add_reporting_period_column(
+        lineage_df=lineage_df,
+        end_date=range_end,
+        previous_weeks_to_include=52,
+        period_size_in_days=14
+    )
+    ## Calculate lineage counts and percentages for each reporting period
+    # Group data to get lineage counts per week
+    counts_by_reporting_period_df = llu.get_lineage_counts_per_period(lineage_df)
     # Add % column to lineage counts table
-    counts_by_week_df = llu.add_percentages_column(counts_by_week_df=counts_by_week_df)
+    counts_by_reporting_period_df = llu.add_percentages_column(counts_by_period_df=counts_by_reporting_period_df)
+    ## Identify lineages to protect
     # Retrieve alias key json from COG-UK website
     pango_aliases_dict = llu.get_pango_aliases(do_filter=True)
-    # Identify lineages to protect
-    lineages_to_protect_list = llu.get_lineages_to_protect(counts_by_week_df=counts_by_week_df,
-                                                           timeframe_length=6,
-                                                           percent_threshold=5,
-                                                           pango_dict=pango_aliases_dict
-                                                           )
-    # Collapse down lineages not in lineages_to_protect_list
-    lc = LineageCollapser(dataframe=counts_by_week_df,
-                          lineages_col='lineage',
-                          totals_col='seq_count',
-                          min_level=2,
-                          protect_lineages=lineages_to_protect_list,
-                          collapsed_col='unaliased_lineage',
-                          pango_aliases=pango_aliases_dict
-                          )
+    # Get reporting periods to protect within
+    reporting_periods = llu.get_periods_to_protect(
+        reporting_periods=counts_by_reporting_period_df['reporting_period'],
+        end_date=range_end,
+        timeframe_length=6
+    )
+    # Identify lineages to protect in last x weeks
+    lineages_to_protect_list = llu.get_lineages_to_protect(
+        counts_by_period_df=counts_by_reporting_period_df,
+        periods_to_protect=reporting_periods,
+        percent_threshold=5,
+        pango_dict=pango_aliases_dict
+    )
     # Combine list of lineages to protect from collapse from each of the processes
     combined_protect = set(lineages_to_protect_list + ['Unassigned'])
-    # Add collapsed_alias column to df for masking so retain lineage_clean column
-    collapsed_week_counts_df = lc.collapsed
-    collapsed_week_counts_df["collapsed_alias"] = collapsed_week_counts_df["unaliased_lineage"]
+    # # Identify lineages to protect in last 52 weeks up until 6 weeks ago
+    # Based on overall prevalence in this period to get number of lineages to protect
+    # up to x value
+    # Collapse down lineages not in lineages_to_protect_list
+    lc = LineageCollapser(dataframe=counts_by_reporting_period_df,
+                          lineages_col='lineage',
+                          totals_col='seq_count',
+                          min_level=1,
+                          protect_lineages=lineages_to_protect_list,
+                          collapsed_col='collapsed_alias',
+                          pango_aliases=pango_aliases_dict
+                          )
+    # Get collapsed alias at correct levels for assigning groups
+    lc.collapse_recursively_to_at_least_n(n=10)
     # Mask anything not in combined_protect list as 'Other'
-    collapsed_masked_week_counts_df = llu.mask_less_prevalent_values(counts_df=collapsed_week_counts_df,
-                                                                     lineages_to_leave_unmasked=dict(
-                                                                        collapsed_alias=combined_protect
-                                                                        )
+    collapsed_masked_counts_df = llu.mask_less_prevalent_values(counts_df=lc.collapsed,
+                                                                     lineages_to_leave_unmasked={
+                                                                        "collapsed_alias" : combined_protect
+                                                                        }
                                                                     )
-    # Add unaliased lineage column to filtered sample dataframe before saving
-    filtered_df['unaliased_lineage'] = lc.alias_to_lineage(filtered_df.lineage)
+    ## Add additional lineage columns to sample info df   
+    # Add unaliased lineage column
+    lineage_df['unaliased_lineage'] = lc.alias_to_lineage(lineage_df.lineage)
+    # Add lineage groups
+    lineage_df = llu.add_lineage_group_to_metadata(lineage_df=lineage_df, counts_df=collapsed_masked_counts_df)
     # Write result files to csv
     # Masked and collapsed lineage week counts
-    llu.write_to_csv(result_df = collapsed_masked_week_counts_df,
+    llu.write_to_csv(result_df = collapsed_masked_counts_df,
                       outdir=out_dir,
                       filename=f"{today_date}_year_lineage_masking.csv")
     # Sample records with unaliased lineage and collapsed alias
-    llu.write_to_csv(result_df = filtered_df,
+    llu.write_to_csv(result_df = lineage_df,
                       outdir=out_dir,
                       filename=f"{today_date}_year_full_lineage_metadata.csv")
+
     # Write to logs if component finished successfully (or not):
     logging.info("Linelist file successfully generated")
 
