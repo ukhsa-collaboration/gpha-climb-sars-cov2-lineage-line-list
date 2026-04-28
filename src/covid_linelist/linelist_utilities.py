533710c947a14ae6c3d0675a701b9f2f500e201f
Deborah Greer <deborah.greer@ukhsa.gov.uk>
2026-04-28 17:23:44 +0100

    feat: Add collapse_and_recalculate_lineage_groups() function to linelist utilities - takes functionality out of get_lineages_to_protect to reduce repetitive code in this function

--- src/covid_linelist/linelist_utilities.py
+++ src/covid_linelist/linelist_utilities.py
@@ -229,6 +229,49 @@ def get_lineages_above_threshold(
     to_protect += over_threshold_list
     return to_protect
 
+def collapse_and_recalculate_lineage_groups(
+    threshold: int,
+    percent_threshold: int,
+    counts_by_period_df: pd.DataFrame,
+    pango_dict: dict,
+    periods_to_protect: list
+    ):
+    """Collapses a dataframe of lineage counts based on given threshold size
+    and then calculates the number of lineage groups over the percent prevalence
+    specified.
+    Arguments:
+        threshold - Minimum group size - lineage groups under this value will be
+                    collapsed into their parent lineage
+        percent_threshold - % of samples of a given lineage in a given period
+                            needed to protect a lineage
+        counts_by_period_df -- Dataframe containing per reporting period counts of lineages
+        pango_dict -- Dict containing pango aliases from COG-UK
+        periods_to_protect -- List of periods to apply collapsing to
+    Outputs:
+        lineage_list -- List of lineages that meet threshold group size and percent
+                        prevalence threshold and will be protected
+    """
+    collapsed_list = []
+    lc = LineageCollapser(
+        dataframe=counts_by_period_df,
+        lineages_col='lineage',
+        totals_col='seq_count',
+        min_level=1,
+        collapsed_col='collapsed_alias',
+        pango_aliases=pango_dict
+        )
+    lc.collapse_based_on_threshold(threshold=threshold)
+    # Get collapsed counts and percentages in each reporting period with new threshold
+    collapsed_period_df = pd.DataFrame(lc.collapsed)[['reporting_period','collapsed_alias', 'seq_count']]
+    collapsed_counts_df = collapsed_period_df.groupby(['reporting_period','collapsed_alias']).sum(numeric_only=True).reset_index()
+    collapsed_counts_df = add_percentages_column(collapsed_counts_df)
+    # Recalculate % prevalences in each time period with new collapsed groups
+    for period in periods_to_protect:
+        collapsed_list += get_lineages_above_threshold(collapsed_counts_df, period, percent_threshold, "collapsed_alias")
+    lineage_list = list(set(collapsed_list))
+
+    return lineage_list
+
 def get_lineages_to_protect(
     counts_by_period_df: pd.DataFrame,
     periods_to_protect: int,
