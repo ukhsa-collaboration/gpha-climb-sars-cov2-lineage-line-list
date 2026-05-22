738cbee9fb8bb13ce65750331365a4ab41e8e5a4
Deborah Greer <63928724+DebGreer@users.noreply.github.com>
2026-05-22 12:13:33 +0100

    Feat/unit tests (#7)
    * test: added test for add_reporting_period_column function.
    
    * test: added test for filter_lineage_df_to_date_cutoff.
    
    * tests: improved test for date_cutoff function.
    
    * tests: added test data.
    
    * feat: Add in additional columns to keep from input csv file. Set dtype for new columns so interpreted as str not int/float
    
    * chore: Update max_lineage values for recent and full reporting window
    
    * chore: Add extra comments to explain threshold starting value and incrementing process in more detail
    
    * docs: Update docs string for add_lineage_group_to_metadata() function in linelist_utilities module
    
    * docs: Update reference to specific number of weeks to be generic, provide specific values as an example of a full reporting window instead
    
    * fix: Change min collapsing level to 1 to allow collapsing back to basal parent lineage
    
    * fix: Update get_periods)to_protect() to correctly return reporting periods
    
    * chore: Subset counts_by_period_df in get_lineages_to_protect as only need calculations for past x weeks/for protected periods, not whole reporting period
    
    * tests: added unit tests for add percs and lineage counts per period.
    
    * tests: Add imports needed for updating test_lineage_collapser script
    
    * tests: Add pango_dict fixture to test_lineage_collapser as pango dict created outside of lineage collapser and passed in now
    
    * tests: Add pango dict as variable to tests in test_lineage_collapser
    
    * tests: Set min collapsing level to 1 in test_lineage_collapser to get expected collapsing back to XEC - otherwise can;t collapse beyond XEC.2 as expected in two of the tests
    
    * tests: Update collapsed column name to collapsed_alias to match current usage
    
    * test: added test for add_reporting_period_column function.
    
    * test: added test for filter_lineage_df_to_date_cutoff.
    
    * tests: improved test for date_cutoff function.
    
    * tests: added test data.
    
    * tests: added unit tests for add percs and lineage counts per period.
    
    * tests: Add pango_dict fixture to test_lineage_collapser as pango dict created outside of lineage collapser and passed in now
    
    * tests: Add pango dict as variable to tests in test_lineage_collapser
    
    * tests: Set min collapsing level to 1 in test_lineage_collapser to get expected collapsing back to XEC - otherwise can;t collapse beyond XEC.2 as expected in two of the tests
    
    * tests: Update collapsed column name to collapsed_alias to match current usage
    
    * fix: Fix merge conflict messages in linelist utilities
    
    * feat: Add function to get total counts for full reporting window
    
    * feat: Update the get_top_lineages_in_full_window function to implement collapsing logic rather than just taking top x lineages before collapsing
    
    * feat: Update run_linelist script to use new logic and functions for getting top lineages in full reporting window
    
    * test: Add tests and fixtures for testing get_top_lineages_in_full_reporting_window
    
    * test: Add test files for testing get_top_lineages_in_full_reporting_window
    
    ---------
    
    Co-authored-by: Jess Friedersdorff <30495397+JessF298@users.noreply.github.com>

--- tests/test_lineage_collapser.py
+++ tests/test_lineage_collapser.py
@@ -8,6 +8,19 @@ from covid_linelist.lineage_collapser import LineageCollapser
 
 
 @pytest.fixture
+def pango_dict():
+    pango_url = "https://raw.githubusercontent.com/cov-lineages/pango-designation/master/pango_designation/alias_key.json"
+    do_filter = True
+    with requests.get(pango_url) as url:
+        pango_aliases_dict = json.loads(url.text)
+        if do_filter:
+            pango_aliases_dict = dict(
+                [t for t in pango_aliases_dict.items() if not t[0].startswith('X') and t[0] not in ['A', 'B']])
+            return pango_aliases_dict
+        else:
+            return pango_aliases_dict
+
+@pytest.fixture
 def test_data():
     return pd.DataFrame(
         {
@@ -41,17 +54,18 @@ def test_data():
 
 
 @pytest.fixture
-def collapser(test_data):
+def collapser(test_data, pango_dict):
     return LineageCollapser(
         dataframe=test_data,
         lineages_col="lineage",
         totals_col="count",
-        min_level=2,
-        collapsed_col="collapsed",
+        min_level=1,
+        collapsed_col="collapsed_alias",
+        pango_aliases=pango_dict,
     )
 
 
-def test_protect_recombinant(test_data):
+def test_protect_recombinant(test_data, pango_dict):
     # if we protect XEC.2, it should remain when count is low
     protected = ["XEC.2"]
     lc = LineageCollapser(
@@ -59,10 +73,12 @@ def test_protect_recombinant(test_data):
         lineages_col="lineage",
         totals_col="count",
         protect_lineages=protected,
+        pango_aliases=pango_dict,
     )
 
     result = lc.collapse_based_on_threshold(threshold=5)
-    assert "XEC.2" in result["collapsed"].values
+
+    assert "XEC.2" in result["collapsed_alias"].values
 
 
 def test_recombinant_collapse(collapser):
@@ -75,58 +91,57 @@ def test_recombinant_collapse(collapser):
 
     result = collapser.collapse_based_on_threshold(threshold=5)
     # XEC.2 should be collapsed to XEC
-    assert "XEC.2" not in result["collapsed"].values
+    assert "XEC.2" not in result["collapsed_alias"].values
     # XEC.2.1.7.15 should be collapsed to XEC
-    assert "XEC.2.1.7.15" not in result["collapsed"].values
+    assert "XEC.2.1.7.15" not in result["collapsed_alias"].values
     # XEC.1.1.1 should be collapsed to XEC
-    assert "XEC.1.1.1" not in result["collapsed"].values
+    assert "XEC.1.1.1" not in result["collapsed_alias"].values
     # XEC.1 should stay
-    assert "XEC.1" in result["collapsed"].values
+    assert "XEC.1" in result["collapsed_alias"].values
     # Base XEC should also stay
-    assert "XEC" in result["collapsed"].values
+    assert "XEC" in result["collapsed_alias"].values
 
 
 def test_percentage_with_recombinants(collapser):
     # total count is 90, so 5% is 4.5
     result = collapser.collapse_based_on_pct(records_pct=5)
-
     # XEC.2 should be collapsed to XEC
-    assert "XEC.2" not in result["collapsed"].values
+    assert "XEC.2" not in result["collapsed_alias"].values
     # XEC.2.1.7.15 should be collapsed to XEC
-    assert "XEC.2.1.7.15" not in result["collapsed"].values
+    assert "XEC.2.1.7.15" not in result["collapsed_alias"].values
     # XEC.1.1.1 should be collapsed to XEC.1
-    assert "XEC.1.1.1" not in result["collapsed"].values
+    assert "XEC.1.1.1" not in result["collapsed_alias"].values
     # XEC.1 should stay
-    assert "XEC.1" in result["collapsed"].values
+    assert "XEC.1" in result["collapsed_alias"].values
     # XEC should stay
-    assert "XEC" in result["collapsed"].values
+    assert "XEC" in result["collapsed_alias"].values
 
 
 def test_standard_lineages_collapse(collapser):
     result = collapser.collapse_based_on_threshold(threshold=5)
 
     # B.1.1 should stay
-    assert "B.1.1" in result["collapsed"].values
+    assert "B.1.1" in result["collapsed_alias"].values
     # B.1.1.7 should stay
-    assert "B.1.1.7" in result["collapsed"].values
+    assert "B.1.1.7" in result["collapsed_alias"].values
     # B.1.1.7.1 should collapse into B.1.1.7
-    assert "B.1.1.7.1" not in result["collapsed"].values
+    assert "B.1.1.7.1" not in result["collapsed_alias"].values
     # B.2 should stay
-    assert "B.2" in result["collapsed"].values
+    assert "B.2" in result["collapsed_alias"].values
     # B.2.1 should collapse into B.2
-    assert "B.2.1" not in result["collapsed"].values
+    assert "B.2.1" not in result["collapsed_alias"].values
 
 
 def test_standard_percentage_collapse(collapser):
     result = collapser.collapse_based_on_pct(records_pct=5)
 
     # B.1.1 should stay
-    assert "B.1.1" in result["collapsed"].values
+    assert "B.1.1" in result["collapsed_alias"].values
     # B.1.1.7 should stay
-    assert "B.1.1.7" in result["collapsed"].values
+    assert "B.1.1.7" in result["collapsed_alias"].values
     # B.1.7.1 should collapse into B.1.1.7
-    assert "B.1.1.7.1" not in result["collapsed"].values
+    assert "B.1.1.7.1" not in result["collapsed_alias"].values
     # B.2 should stay
-    assert "B.2" in result["collapsed"].values
+    assert "B.2" in result["collapsed_alias"].values
     # B.2.1 should collapse into B.2
-    assert "B.2.1" not in result["collapsed"].values
+    assert "B.2.1" not in result["collapsed_alias"].values
---
+++ tests/test_linelist_utilities.py
@@ -0,0 +1,172 @@
+from datetime import date
+from pathlib import Path
+
+import pandas as pd
+import pytest
+import requests
+import json
+
+from covid_linelist import linelist_utilities as ull
+
+TEST_DATA_PATH = Path(Path(__file__).resolve().parent / "test_data")
+
+END_DATE = date(2026, 3, 30)
+
+
+@pytest.fixture
+def test_lineage_df():
+    lineage_df = pd.read_csv(
+        Path(TEST_DATA_PATH / "test_data.csv"),
+        parse_dates=["collection_date", "expected_period"],
+        dayfirst=True,
+    )
+    lineage_df["collection_date"] = pd.to_datetime(
+        lineage_df["collection_date"].dt.strftime("%Y%m%d")
+    )
+    return lineage_df
+
+@pytest.fixture
+def counts_df_one():
+    counts_df = pd.read_csv("tests/test_data/totals_df.csv")
+    return counts_df
+
+@pytest.fixture
+def expected_list_one():
+    lineage_list = ["RE.1.1", "XBB.1.5", "XDV.1"]
+    return lineage_list
+
+@pytest.fixture
+def counts_df_two():
+    counts_df = pd.read_csv("tests/test_data/totals_df_v2.csv")
+    return counts_df
+
+@pytest.fixture
+def expected_list_two():
+    lineage_list = ["RE.1.1", "RF.1", "RT.1", "XBB.1", "XDV.1",  "XFN"]
+    return lineage_list
+
+@pytest.fixture
+def pango_dict():
+    pango_url = "https://raw.githubusercontent.com/cov-lineages/pango-designation/master/pango_designation/alias_key.json"
+    do_filter = True
+    with requests.get(pango_url) as url:
+        pango_aliases_dict = json.loads(url.text)
+        if do_filter:
+            pango_aliases_dict = dict(
+                [t for t in pango_aliases_dict.items() if not t[0].startswith('X') and t[0] not in ['A', 'B']])
+            return pango_aliases_dict
+        else:
+            return pango_aliases_dict
+
+def test_add_reporting_period_column(test_lineage_df):
+    """
+    This function adds a new column that rounds the collection date to the start date of whatever reporting period is
+    defined. The reporting period size is given in days, and the reporting period start dates are given that many days
+    apart. So with a period size of 7 days and an end range date of Monday 30th March, the reporting dates would be
+    30th, 23rd, 16th, 9th in March, and so on, for as many weeks is defined by 'previous_weeks_to_include'.
+    """
+    ## test data has samples collected from 02/12/2025 - 31/03/2026, a range of <20 weeks
+    ## Get Monday of current week as end of range for any time periods calculated.
+    actual = ull.add_reporting_period_column(
+        lineage_df=test_lineage_df,
+        end_date=END_DATE,
+        previous_weeks_to_include=20,
+        period_size=7,  # days
+    )
+    # Print an example to explain the test before assertion
+    eg_date = "2026-03-31"
+    eg = (
+        actual.loc[actual["collection_date"] == eg_date, "reporting_period"]
+        .dt.strftime("%Y-%m-%d")
+        .values[0]
+    )
+    print(
+        f"Test add_reporting_period_column - expect sample with collection date {eg_date} to get reporting "
+        f"period 2026-03-30, got {eg}"
+    )
+
+    assert actual["reporting_period"].equals(actual["expected_period"]), (
+        f'Expected the column "reporting_period" created by the function to match "expected_period" in the test data,'
+        f"but these rows do not match:\n{actual.loc[~actual['expected_period'] == actual['reporting_period']]}"
+    )
+
+
+def test_filter_lineage_df_to_date_cutoff(test_lineage_df):
+    """
+    This functions filters the lineage df based on date range which is between a specified filter_end_date and the
+    number of previous_weeks_to_include. Date must not be str, must be datetime object
+    Noting that the function rolls the date back to the Monday of the week, then goes back the provided number of weeks.
+    """
+    # The date 10 weeks ago is the 19th Jan 2026, there are 12 samples with a collection date later than that.
+    filtered_lineage_df = ull.filter_lineage_df_to_date_cutoff(
+        lineage_df=test_lineage_df, filter_end_date=END_DATE, previous_weeks_to_include=10
+    )
+    assert (l := len(filtered_lineage_df)) == 12, f"Expected 12 samples after filtering, got {l}"
+
+    # The function rolls back to Monday before finding the date to filter to the provided number of weeks previous. When
+    # providing a non-Monday date in the same week, should get the same return
+
+    filtered_lineage_df_2 = ull.filter_lineage_df_to_date_cutoff(
+        lineage_df=test_lineage_df, filter_end_date=date(2026, 4, 1), previous_weeks_to_include=10
+    )
+    assert filtered_lineage_df.equals(filtered_lineage_df_2), (
+        "Expected these dataframes to match - check that the function rolls back to Monday before filtering."
+    )
+
+
+def test_lineage_counts_per_period(test_lineage_df):
+    """Get the counts of lineages in a reporting period"""
+    test_lineage_df = test_lineage_df.rename(columns={"expected_period": "reporting_period"})
+
+    counts = ull.get_lineage_counts_per_period(test_lineage_df)
+
+    assert counts.iloc[12]["seq_count"] == 3
+    assert counts.iloc[10]["seq_count"] == 2
+
+    print(
+        "Expected 2 counts of XFG.1.1.1 in period 2026-03-16 (row 10) and 3 counts of XFG.1.1 in "
+        "period 2026-03-23 (row 12). "
+        f"\ngot:\n{counts}"
+    )
+
+
+def test_add_percentages_column():
+    counts_by_period = pd.DataFrame(
+        data=[
+            ["2026-03-23", "KP.2.3", 1, 10.0],
+            ["2026-03-23", "XFG.1.1", 3, 30.0],
+            ["2026-03-23", "XFG.1.1.1", 5, 50.0],
+            ["2026-03-23", "KP.2", 1, 10.0],
+            ["2026-02-09", "KP.2.3", 1, 20.0],
+            [
+                "2026-02-09",
+                "XFG.1.1",
+                1,
+                20.0,
+            ],
+            ["2026-02-09", "XFG.1.1.1", 3, 60.0],
+        ],
+        columns=["reporting_period", "lineage", "seq_count", "expected_pct"],
+    )
+
+    df = ull.add_percentages_column(counts_by_period)
+    assert pd.Series.equals(df["pct_of_reporting_period"], df["expected_pct"])
+
+@pytest.mark.parametrize(
+    "counts_df, pango_aliases, expected_list",
+    [
+        ("counts_df_one", "pango_dict", "expected_list_one"),
+        ("counts_df_two", "pango_dict", "expected_list_two")
+    ],
+)
+def test_get_top_lineages_in_full_window(counts_df, pango_aliases, expected_list, request):
+    counts_df = request.getfixturevalue(counts_df)
+    pango_aliases = request.getfixturevalue(pango_aliases)
+    expected_list = request.getfixturevalue(expected_list)
+
+    already_protected = ["BA.3.2", "BA.3.2.2", "RE.1.1.2", "LF.1", "XFG.1"]
+    collapse_limit = ["BA.2.86", "BA.2", "BA.3", "JN.1"]
+    lineage_list = ull.get_top_lineages_in_full_window(counts_df, already_protected, collapse_limit, pango_aliases, 6)
+    print(lineage_list)
+
+    assert lineage_list == expected_list
